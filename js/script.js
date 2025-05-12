4fd4c56082eca017dc8a6ad37fe1a1dbe981b9c7
printericlee <xyxz55124019@gmail.com>
2025-05-13 02:26:13 +0800

    homepage

--- js/script.js
+++ js/script.js
@@ -1,32 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // 導航列下拉選單切換 (觸控裝置)
-    const navItems = document.querySelectorAll('#nav > ul > li');
+$(document).ready(function () {
 
-    navItems.forEach(item => {
-        item.addEventListener('click', (e) => {
-            const submenu = item.querySelector('ul');
-            if (submenu) {
-                e.preventDefault();
-                submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
-            }
-        });
-    });
+    $("#collapse").on("click", function () {
 
-    // 深色模式切換
-    const toggleBtn = document.getElementById('mode-toggle');
-    const body = document.body;
+        $("#sidebar").toggleClass("active");
+        $(".fa-left-long").toggleClass("fa-right-long");
 
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        body.classList.add('dark-mode');
-    }
-
-    toggleBtn.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-
-        if (body.classList.contains('dark-mode')) {
-            localStorage.setItem('darkMode', 'enabled');
-        } else {
-            localStorage.setItem('darkMode', 'disabled');
-        }
-    });
-});
+    })
+})
